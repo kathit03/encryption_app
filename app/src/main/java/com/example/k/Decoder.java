f392b777de0f60ec04182582ec7e4c7bd3a24a5d
kathit03 <kathitshah007@gmail.com>
2023-01-20 22:03:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/k/Decoder.java
@@ -0,0 +1,48 @@
+package com.example.k;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Decoder extends AppCompatActivity {
+    EditText etdec;
+    TextView dectv;
+    ClipboardManager cplboard;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decoder);
+
+        etdec=findViewById(R.id.etdec);
+        dectv=findViewById(R.id.dectv);
+        cplboard=(ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
+
+    public void dec(View view)
+    {
+        String temp=etdec.getText().toString();
+        String rv=decode.dec(temp);
+        dectv.setText(rv);
+    }
+
+    public void cp1(View view) {
+        String data = dectv.getText().toString().trim();
+        if (!data.isEmpty()) {
+            ClipData temp = ClipData.newPlainText("text", data);
+            cplboard.setPrimaryClip(temp);
+            Toast.makeText(this, "copied", Toast.LENGTH_LONG).show();
+        }
+    }
+}
